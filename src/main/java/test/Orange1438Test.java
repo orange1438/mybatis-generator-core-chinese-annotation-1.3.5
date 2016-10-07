ccbdb4c49692eea3ee4d3b13fb33271951101504
orange1438 <orange1438@qq.com>
2016-10-07 17:30:21 +0800

    添加测试和测试用的配置文件

---
+++ src/main/java/test/Orange1438Test.java
@@ -0,0 +1,28 @@
+package test;
+
+import org.mybatis.generator.api.ShellRunner;
+
+/**
+ * @author orange1438
+ *         2016/10/07 16:58
+ */
+public class Orange1438Test {
+
+
+    public static void main(String[] args) throws Exception {
+
+        // 调试初始化参数
+        Orange1438Test test = new Orange1438Test();
+        //取得根目录路径
+        String rootPath = test.getClass().getResource("/").getFile().toString();
+        //当前目录路径
+        //String currentPath1=test.getClass().getResource(".").getFile().toString();
+        //String currentPath2=test.getClass().getResource("").getFile().toString();
+        //当前目录的上级目录路径
+        //   String parentPath=test.getClass().getResource("../").getFile().toString();
+        String[] arg = new String[]{"-configfile", rootPath + "test/generatorConfig.xml", "-overwrite"};
+
+        ShellRunner.main(arg);
+
+    }
+}
